a6b5db8f2388667d44740ffdc3d3db3a873313e6
Yane <yanelisset4@gmail.com>
2021-03-01 00:30:44 -0300

    feat(category): add category api

---
+++ src/app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Http\Requests\StoreUpdateCategoryFormRequest;
+
+class CategoryController extends Controller
+{
+    private $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $categories = $this->category->get();
+        return response()->json($categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUpdateCategoryFormRequest $request)
+    {
+        $category = $this->category->create($request->all());
+        return response()->json($category, 201);
+    }
+
+    /**
+     * Show the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function show($id)
+    {
+        $category = $this->category->find($id);
+        if (!$category)
+            return response()->json(['error' => 'Not found'], '404');
+        return response()->json($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreUpdateCategoryFormRequest $request, $id)
+    {
+        $category = $this->category->find($id);
+        if (!$category) {
+            return response()->json(['error' => 'Not found'], '404');
+        } else {
+            $category->update($request->all());
+            return response()->json($category);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category = $this->category->find($id);
+        if (!$category)
+            return response()->json(['error' => 'Not found'], '404');
+
+        $category->delete();
+        return response()->json(['sucess' => true], 204);
+    }
+}
